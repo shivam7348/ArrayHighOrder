0619ad483ac8a680eb3655c4e120b46a188272dc
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-12-25 18:52:25 +0530

    object

---
+++ Arrow.js
@@ -0,0 +1,22 @@
+// function coding( num1 , num2 ) {
+//   return ( num1 + num2)
+// }
+//  console.log(coding( 3 + 5));
+  
+const person = {
+    name: 'John',
+    age: 34,
+    job: 'Developer'
+
+};
+
+function printPersonDetails(personObject) {
+   console.log(`Age: ${personObject.age}`)
+   console.log(`Name ${personObject.name}`)
+   console.log(`Job ${personObject.job}`)
+
+}
+
+printPersonDetails(person)
+ 
+
